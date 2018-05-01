f1c4eb7061c206e1776dde2e43b3f33b186e09e9
omokehinde <engr.omokehinde@gmail.com>
2018-05-01 13:13:21 +0100

    Test code refactor

--- server/test/cache.js
+++ server/test/cache.js
@@ -1,24 +1,25 @@
 process.env.NODE_ENV = 'test';
 
-let mongoose = require('mongoose');
-let CachedData = require('../models/cachedData'); 
+const mongoose = require('mongoose');
+const CachedData = require('../models/cachedData'); 
 
-let chai = require('chai');
-let chaiHttp = require('chai-http');
-let server = require('../server');
-let should = chai.should();
+const chai = require('chai.request(server)');
+const chaiHttp = require('chai-http');
+const server = require('../server');
+const should = chai.should();
 
 chai.use(chaiHttp);
 
+const requester = chai.request(server).keepOpen()
+
 describe('/GET all Cached data', () => {
     it('it should GET all CachedData', (done) => {
-        chai.request(server)
-            .get('/')
+        requester
+            .get('/api/v1')
             .end((err, res) => {
                 res.should.have.status(200);
                 res.body.should.be.a('array');
                 res.body.length.should.be.eql(0);
-
                 done();
             });
     });
